be8be1d0b9e08b8602794c4a5b942c452d84e16e
DrStang <48525932+DrStang@users.noreply.github.com>
2026-01-09 21:05:13 -0500

    Update warm-cache.js

--- warm-cache.js
+++ warm-cache.js
@@ -18,6 +18,7 @@ async function warmCache() {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME || 'Goodreads',
     connectionLimit: 5
   });
